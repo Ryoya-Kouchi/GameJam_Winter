69bff6782f3647879ac9f57915778c6b2c926584
Shingo-sueyoshi <138412610+Shingo-sueyoshi@users.noreply.github.com>
2024-02-13 13:59:44 +0900

    ソースファイル書き写し

--- GameJam_Winter/AbstractScene.h
+++ GameJam_Winter/AbstractScene.h
@@ -1,15 +1,39 @@
 #pragma once
+
+//SceneBase.hの代わり
+
+//シーン情報
+enum eAbstractSceneType
+{
+	E_TITLE,
+	E_MAIN,
+	E_HELP,
+	E_RANKING_DISP,
+	E_RANKING_INPUT,
+	E_RESULT,
+	E_END
+};
+
 // デフォルトの抽象シーンクラス
 // ユーザーはこれを継承してシーンを実装すると楽
-
 class AbstractScene {
 public:
+	AbstractScene() {}
     // デストラクタ
     virtual ~AbstractScene() {}
 
     // 更新処理を実装する
-    virtual AbstractScene* Update() = 0;
+	virtual eAbstractSceneType Update()
+	{
+		return GetNowScene();
+	}
 
     // 描画処理を実装する
-    virtual void Draw() const = 0;
+    virtual void Draw() const {}
+
+	//終了時処理
+	virtual void Finalize() {}
+
+	//現在シーン情報を取得
+	virtual eAbstractSceneType GetNowScene() const = 0;
 };
--- GameJam_Winter/Help.h
+++ GameJam_Winter/Help.h
@@ -1,4 +1,24 @@
 #pragma once
+
+#include "SceneBase.h"
+
+class HelpScene :public SceneBase
+{
+private:
+	int background_image;		//”wŒi‰æ‘œ
+
+public:
+	HelpScene();
+	virtual~HelpScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eSceneType GetNowScene() const override;
+
+}; #pragma once
 #include"AbstractScene.h"
 class Help : public AbstractScene
 {
